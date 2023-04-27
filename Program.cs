84bca91aeef80155156af45fe0c27e938f77acaf
Boom Fruits <you@example.com>
2023-04-27 09:56:01 +0700

    temp

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap so sinh vien: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Sinhvien[] sv = new Sinhvien[n];
+            for(int i = 0; i < n; i++)
+            {
+                sv[i] = new Sinhvien();
+                Console.WriteLine("Nhap thong tin sinh vien thu " + i + 1);
+                Console.Write("Nhap msv: ");
+                sv[i].Msv = Console.ReadLine();
+                Console.Write("Nhap ho ten: ");
+                sv[i].Hoten = Console.ReadLine();
+                Console.Write("Nhap diem toan: ");
+                sv[i].DiemToan = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Nhap diem ly: ");
+                sv[i].DiemLy = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Nhap diem hoa: ");
+                sv[i].DiemHoa = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine("\n ====XUAT DS SINH VIEN====");
+            foreach (Sinhvien s in sv)
+                s.show();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sinhvien.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai5
+{
+    class Sinhvien
+    {
+        private string msv;
+        private string hoten;
+        private double diemToan;
+        private double diemLy;
+        private double diemHoa;
+        public string Msv
+        {
+            get { return msv; }
+            set { msv = value; }
+        }
+        public string Hoten
+        {
+            get { return hoten; }
+            set { hoten = value; }
+        }
+        public double DiemToan
+        {
+            get { return diemToan; }
+            set { diemToan = value; }
+        }
+        public double DiemLy
+        {
+            get { return diemLy; }
+            set { diemLy = value; }
+        }
+        public double DiemHoa
+        {
+            get { return diemHoa; }
+            set { diemHoa = value; }
+        }
+        public void show()
+        {
+            Console.WriteLine("MSSV:{0}",msv);
+            Console.WriteLine("Ten SV:{0}",hoten);
+            Console.WriteLine("Diem Toan:{0}", diemToan);
+            Console.WriteLine("Diem Ly:{0}", diemLy);
+            Console.WriteLine("Diem Hoa:{0}", diemHoa);
+        }
+    }
+}
